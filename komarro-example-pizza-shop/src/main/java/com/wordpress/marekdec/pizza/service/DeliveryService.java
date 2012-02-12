c24c88f10c0796e624163657a2cca2f21f67d44f
marekdec@gmail.com <marekdec@gmail.com>
2012-02-12 22:50:54 +0100

    DeliveryService implemented

--- komarro-example-pizza-shop/src/main/java/com/wordpress/marekdec/pizza/service/DeliveryService.java
+++ komarro-example-pizza-shop/src/main/java/com/wordpress/marekdec/pizza/service/DeliveryService.java
@@ -1,12 +1,24 @@
 package com.wordpress.marekdec.pizza.service;
 
+import javax.inject.Inject;
+
 import org.joda.time.DateTime;
 
 import com.wordpress.marekdec.pizza.domain.Address;
 
 public class DeliveryService {
 
+	@Inject
+	private PizzaService pizzaService;
+
+	@Inject
+	private MapService mapService;
+
 	public DateTime getEstimatedTimeOfDeliveryTo(long orderId, Address address) {
-		return null;
+		DateTime timeWhenPizzaIsReady = pizzaService
+				.whenWillBePizzaReady(orderId);
+
+		int minutesToGetToGivenAddress = mapService.getTimeFromHereTo(address);
+		return timeWhenPizzaIsReady.plusMinutes(minutesToGetToGivenAddress);
 	}
 }
