cd5d66189297bbeae51f95b7c2dadeeaf3808b15
marekdec@gmail.com <marekdec@gmail.com>
2012-02-12 23:50:57 +0100

    OderService implemented

--- komarro-example-pizza-shop/src/main/java/com/wordpress/marekdec/pizza/service/OrderService.java
+++ komarro-example-pizza-shop/src/main/java/com/wordpress/marekdec/pizza/service/OrderService.java
@@ -1,10 +1,34 @@
 package com.wordpress.marekdec.pizza.service;
 
+import javax.inject.Inject;
+
 import com.wordpress.marekdec.pizza.domain.Address;
+import com.wordpress.marekdec.pizza.domain.Order;
+import com.wordpress.marekdec.pizza.domain.Pizza;
+import com.wordpress.marekdec.pizza.repository.OrderRepository;
 
 public class OrderService {
 
+	@Inject
+	private OrderRepository orderRepository;
+
+	@Inject
+	private PizzaService pizzaService;
+
 	public long placeOrder(String pizzaName, Address address) {
-		return 0;
+		Pizza pizza = pizzaService.getByName();
+
+		if (pizza != null) {
+			Order order = new Order();
+			order.setAddress(address);
+			order.setPizza(pizza);
+
+			Order newOrder = orderRepository.save(order);
+
+			return newOrder.getId();
+		} else {
+			throw new IllegalArgumentException("The pizza " + pizzaName
+					+ " does not exist in the menu card");
+		}
 	}
 }
